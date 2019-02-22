547e3cf66d4ff0234f59a76f068890e36f66692d
Elmar Wachtmeester <elmar.wachtmeester@ing.nl>
2019-02-22 22:59:58 +0100

    Day 2 more generic

--- src/aoc2018/day2.clj
+++ src/aoc2018/day2.clj
@@ -1,23 +1,24 @@
 (ns aoc2018.day2)
 
-;;TODO can make check for two and three generic with map
-
-(defn part1 [input] (reduce * (reduce (fn [acc vec] (map + acc vec))
-                                      (map (fn [id]
-                                               (map #(if % 1 0)                           ;; turn boolean in 1 and 0
-                                                    (reduce                               ;; check if id has 2s or 3s ; returns vector of booleans
-                                                      (fn [[has_two has_three], [char count]]
-                                                          [(or has_two (= count 2)), (or has_three (= count 3))])
-                                                      []
-                                                      (reduce                             ;; count occurences (per id) ; returns map of char -> count
-                                                        (fn [a, c]
-                                                            (update
-                                                              a
-                                                              (keyword (str c))
-                                                              (fn [x] (+ 1 (or x 0)))))
-                                                        {}
-                                                        (seq id))
-                                                      ))) (clojure.string/split-lines input)))))
+(defn part1
+      ([input] (part1 input [2 3]))
+      ([input check]
+              (reduce *
+                      (reduce
+                        #(map + %1 %2)
+                        (map
+                          (fn [id]
+                              (map #(if % 1 0)              ;; turn boolean in 1 and 0
+                                   (reduce                  ;; check if id has check counts ; returns vector of booleans
+                                     (fn [arr [char count]] (map #(or %1 (= count %2)) arr check))
+                                     (map (fn[x] false) check)
+                                     (reduce                ;; count occurences (per id) ; returns map of char -> count
+                                       (fn [a, c]
+                                           (update a (keyword (str c)) #(inc (or % 0))))
+                                       {}
+                                       (seq id))
+                                     )))
+                          (clojure.string/split-lines input))))))
 
 (assert (= (part1 "abcdef
 bababc
@@ -26,10 +27,6 @@ abcccd
 aabcdd
 abcdee
 ababab") 12))
-
-
-
-
 
 (part1 (slurp "resources/day2.txt")) ;; 8398
 
@@ -60,21 +57,3 @@ wvxyz") "fgij"))
 
 (part2 (slurp "resources/day2.txt"))                        ;; hhvsdkatysmiqjxunezgwcdpr
 
-
-
-(reduce * (reduce (fn [acc vec] (map + acc vec))
-        (map (fn [id]
-                 (map #(if % 1 0)                           ;; turn boolean in 1 and 0
-                      (reduce                               ;; check if id has 2s or 3s ; returns vector of booleans
-                        (fn [[has_two has_three], [char count]]
-                            [(or has_two (= count 2)), (or has_three (= count 3))])
-                        []
-                        (reduce                             ;; count occurences (per id) ; returns map of char -> count
-                          (fn [a, c]
-                              (update
-                                a
-                                (keyword (str c))
-                                (fn [x] (+ 1 (or x 0)))))
-                          {}
-                          (seq id))
-                        ))) ["abcdef" "bababc" "abbcde" "abcccd" "aabcdd" "abcdee" "ababab"])))
