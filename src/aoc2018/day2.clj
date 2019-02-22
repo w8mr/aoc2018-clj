ae9a28b49b3042198c3b0186c6847411ca48489e
Elmar Wachtmeester <elmar.wachtmeester@ing.nl>
2019-02-22 16:23:13 +0100

    Used nested vars instead of first and second

--- src/aoc2018/day2.clj
+++ src/aoc2018/day2.clj
@@ -1,22 +1,24 @@
 (ns aoc2018.day2)
 
+;;TODO can make check for two and three generic with map
 
-(defn part1 [input] (let [value (reduce
-                                  (fn [acc, id]
-                                      (let [value
-                                            (reduce
-                                              (fn [acc, kv]
-                                                  [(or (first acc) (= (second kv) 2)), (or (second acc) (= (second kv) 3))])
-                                              [false, false]
-                                              (reduce
+(defn part1 [input] (let [value (reduce ;; count amount of 2s and 3s over all ids
+                                  (fn [[count_two count_three], id]
+                                      (let [[has_two has_three]
+                                            (reduce ;; check if id has 2s or 3s
+                                              (fn [[has_two has_three], [char count]]
+                                                  [(or has_two (= count 2)), (or has_three (= count 3))])
+                                              []
+                                              (reduce ;; count occurences (per id)
                                                 (fn [a, c]
                                                     (update
                                                       a
                                                       (keyword (str c))
                                                       (fn [x] (+ 1 (or x 0)))))
                                                 {}
-                                                (seq id)))]
-                                           [(+ (first acc) (if (first value) 1 0)) (+ (second acc) (if (second value) 1 0))]))
+                                                (seq id))
+                                              )]
+                                           [(+ count_two (if has_two 1 0)) (+ count_three (if has_three 1 0))]))
                                   [0 0]
                                   (clojure.string/split-lines input))]
                          (* (first value) (second value))))
