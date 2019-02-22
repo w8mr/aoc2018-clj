15bc460b7e67c153797ad65a8c229a9e145a5cdb
Elmar Wachtmeester <elmar.wachtmeester@ing.nl>
2019-02-22 17:07:10 +0100

    Used more maps and reduces to make it more generic Day 2

--- src/aoc2018/day2.clj
+++ src/aoc2018/day2.clj
@@ -2,35 +2,33 @@
 
 ;;TODO can make check for two and three generic with map
 
-(defn part1 [input] (let [value (reduce ;; count amount of 2s and 3s over all ids
-                                  (fn [[count_two count_three], id]
-                                      (let [[has_two has_three]
-                                            (reduce ;; check if id has 2s or 3s
-                                              (fn [[has_two has_three], [char count]]
-                                                  [(or has_two (= count 2)), (or has_three (= count 3))])
-                                              []
-                                              (reduce ;; count occurences (per id)
-                                                (fn [a, c]
-                                                    (update
-                                                      a
-                                                      (keyword (str c))
-                                                      (fn [x] (+ 1 (or x 0)))))
-                                                {}
-                                                (seq id))
-                                              )]
-                                           [(+ count_two (if has_two 1 0)) (+ count_three (if has_three 1 0))]))
-                                  [0 0]
-                                  (clojure.string/split-lines input))]
-                         (* (first value) (second value))))
+(defn part1 [input] (reduce * (reduce (fn [acc vec] (map + acc vec))
+                                      (map (fn [id]
+                                               (map #(if % 1 0)                           ;; turn boolean in 1 and 0
+                                                    (reduce                               ;; check if id has 2s or 3s ; returns vector of booleans
+                                                      (fn [[has_two has_three], [char count]]
+                                                          [(or has_two (= count 2)), (or has_three (= count 3))])
+                                                      []
+                                                      (reduce                             ;; count occurences (per id) ; returns map of char -> count
+                                                        (fn [a, c]
+                                                            (update
+                                                              a
+                                                              (keyword (str c))
+                                                              (fn [x] (+ 1 (or x 0)))))
+                                                        {}
+                                                        (seq id))
+                                                      ))) (clojure.string/split-lines input)))))
 
-(assert (= (part1 "
-abcdef
+(assert (= (part1 "abcdef
 bababc
 abbcde
 abcccd
 aabcdd
 abcdee
 ababab") 12))
+
+
+
 
 
 (part1 (slurp "resources/day2.txt")) ;; 8398
@@ -60,5 +58,23 @@ fguij
 axcye
 wvxyz") "fgij"))
 
-(part2 (slurp "resources/day2.txt")) ;; hhvsdkatysmiqjxunezgwcdpr
+(part2 (slurp "resources/day2.txt"))                        ;; hhvsdkatysmiqjxunezgwcdpr
 
+
+
+(reduce * (reduce (fn [acc vec] (map + acc vec))
+        (map (fn [id]
+                 (map #(if % 1 0)                           ;; turn boolean in 1 and 0
+                      (reduce                               ;; check if id has 2s or 3s ; returns vector of booleans
+                        (fn [[has_two has_three], [char count]]
+                            [(or has_two (= count 2)), (or has_three (= count 3))])
+                        []
+                        (reduce                             ;; count occurences (per id) ; returns map of char -> count
+                          (fn [a, c]
+                              (update
+                                a
+                                (keyword (str c))
+                                (fn [x] (+ 1 (or x 0)))))
+                          {}
+                          (seq id))
+                        ))) ["abcdef" "bababc" "abbcde" "abcccd" "aabcdd" "abcdee" "ababab"])))
